07e3f787cd62f56a4df6d6974279d54360d128a3
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2024-10-02 12:36:20 +0700

    fixed8

--- src/routes/_route.js
+++ src/routes/_route.js
@@ -5,6 +5,9 @@ function route(app) {
     app.use('/api', api);
     app.use('/realtime/api', realTimeApi);
 
+    app.get('*', (req, res) => {
+        res.redirect('/');
+    });
 }
 
 module.exports = route;
